2441615677e98bb9cf65d9055081af5c24397c6e
Admilson Landim <ady.pina10@gmail.com>
2020-11-20 14:08:42 -0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,12 @@ $(document).ready(function() {
     setup_nivo_lightbox();
     setup_dense();
 
+
     $(window).load(function() {
         $(".js-preloader").fadeOut(800, function() {
             $(".js-main-container").fadeIn(800);
 
-            setup_scrollreveal();
+            // setup_scrollreveal();
             setup_progress_bar_animation();
         });
     });
